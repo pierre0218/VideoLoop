be93fce785cfcbc935df3250a92986273ada942b
sean2249 <sean22492249@gmail.com>
2016-06-07 12:19:26 +0800

    add gray

--- createloop.m
+++ createloop.m
@@ -2,11 +2,18 @@ v = VideoReader('palmtrees.mp4');
 
 totalFrames = floor(v.Duration * v.FrameRate);
 
+diff = 0;
 for i=1:totalFrames-1
-f1 = read(v,i);
-f2 = read(v,i+1);
-diff = diff + f2-f1;
+    f1 = rgb2gray(read(v,i));
+    f2 = rgb2gray(read(v,i+1));
+    diff = diff + f2-f1;
 end
 
+[rowDiff, colDiff]=find(diff>5);
+totalPixel = size(diff,1)*size(diff,2);
+loopPixel = size(rowDiff,1);
+staticPixel = totalPixel - loopPixel;
+fprintf('Total Pixel= %d, Loop Pixel= %d, zeros Pixel= %d\n',...
+    totalPixel, loopPixel, staticPixel);
 
 imshow(diff);
